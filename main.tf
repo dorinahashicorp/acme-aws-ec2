c8d5f4596b9776b250ef281245b100bad1bdce95
Dorina Timbur <94868032+dorinahashicorp@users.noreply.github.com>
2024-05-22 10:29:47 +0100

    Update main.tf
    Add AMI data source

--- main.tf
+++ main.tf
@@ -2,8 +2,23 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]  // Canonical's owner ID
+}
+
 resource "aws_instance" "example" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   tags = {
     Name = "ACME-EC2-Test"
