d4ce3503c82abd297b9c7ae53a6b3e06881069bd
Dorina Timbur <94868032+dorinahashicorp@users.noreply.github.com>
2024-05-22 11:31:24 +0100

    Update variables.tf
    remove AMI variable

--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
 variable "aws_region" {
   description = "AWS region where the EC2 instance will be created"
   type        = string
-}
-
-variable "ami_id" {
-  description = "AMI ID to use for the EC2 instance"
-  type        = string
+  default     = "us-east-1"  # Example default region
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance (e.g., t2.micro)"
   type        = string
+  default     = "t2.micro"  # Example default instance type
 }
